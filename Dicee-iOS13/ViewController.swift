722e1fc72ecb88c92e6c25415890c5060ed4998b
smease12 <scmease93@gmail.com>
2023-12-09 15:07:44 -0500

    Update diceImageView1 to dice six on app load

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix");
     }
 
 
