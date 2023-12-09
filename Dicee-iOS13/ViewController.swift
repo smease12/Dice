cddcf3c5660e49993e6b80686e79e12d368b1732
smease12 <scmease93@gmail.com>
2023-12-09 15:30:55 -0500

    Add rollButtonPressed IBAction

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,13 +12,19 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
     
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix");
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo");
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button got tapped.")
+    }
+    
 }
 
