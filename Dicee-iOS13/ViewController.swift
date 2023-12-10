c587e93317a34742860dedce0effbc1d4c9ef692
smease12 <scmease93@gmail.com>
2023-12-10 18:11:21 -0500

    Add dice images array

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,17 +14,24 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix");
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo");
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceFour");
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceFour");
+        let diceArray =
+       [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"),
+        UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"),
+        UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray[leftDiceNumber]
+        diceImageView2.image = diceArray[rightDiceNumber]
+        
     }
     
 }
